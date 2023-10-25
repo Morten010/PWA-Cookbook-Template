5619f4c1802f6c9b57eabb53f116bbb858bdff59
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2023-10-25 10:46:25 +0200

    d

--- workers/sw.js
+++ workers/sw.js
@@ -1,11 +1,27 @@
 //naming of cache
 const staticCacheName = "staticCache1.0"
+const dynamicCacheName = 'site-dynamic-v1'
 
 const assets = [
 	"/",
 	"/index.html",
-	"/css/styles.css"
+	"/css/styles.css",
+	"/pages/fallback.html"
 ]
+
+const limitCacheSize = (cacheName, numberOfAllowedFiles) => {
+	// Åbn den angivede cache
+	caches.open(cacheName).then(cache => {
+		// Hent array af cache keys 
+		cache.keys().then(keys => {
+			// Hvis mængden af filer overstiger det tilladte
+			if(keys.length > numberOfAllowedFiles) {
+				// Slet første index (ældste fil) og kør funktion igen indtil antal er nået
+				cache.delete(keys[0]).then(limitCacheSize(cacheName, numberOfAllowedFiles))
+			}
+		})
+	})
+}
 
 // register servise worker
 if('serviceWorker' in navigator) {
@@ -28,15 +44,16 @@ self.addEventListener('activate', event => {
 	console.log('Service Worker has been activated');
 	event.waitUntil(
 		caches.keys().then(keys => {
-			filteredKeys = keys.filter(key => key !== staticCacheName)
-			filteredKeys.maps(key => caches.delete(key))
+			const filteredKeys = keys.filter(key => key !== staticCacheName)
+			console.log(filteredKeys);
+			filteredKeys.map(key => caches.delete(key))
 		})
 	)
 })
 
 // Fetch event
 self.addEventListener('fetch', event => {
-	console.log('Fetch event', event)
+	console.log(event);
 	if(!(event.request.url.indexOf('http') === 0)) return
 
 	event.respondWith(
@@ -44,6 +61,29 @@ self.addEventListener('fetch', event => {
 			caches.put(event.put(event.request.url, fetchRes.clone()))
 
 			return fetchRes
+		}).catch(() => {
+			return caches.match('/pages/fallback.html')
 		})
 	)
+
+	// dynamisk cache
+	// event.respondWith(
+	// 	// Kig efter file match i cache 
+	// 	caches.match(event.request).then(cacheRes => {
+	// 		// Returner match fra cache - ellers hent fil på server
+	// 		return cacheRes || fetch(event.request).then(fetchRes => {
+	// 			// Tilføjer nye sider til cachen
+	// 			return caches.open(dynamicCacheName).then(cache => {
+	// 				// Bruger put til at tilføje sider til vores cache
+	// 				// Læg mærke til metoden clone
+	// 				cache.put(event.request.url, fetchRes.clone())
+	// 				// Returnerer fetch request
+	// 				return fetchRes
+	// 			})
+	// 		})
+	// 	})
+	// )
+
+	limitCacheSize(staticCacheName, 8)
+
 })
